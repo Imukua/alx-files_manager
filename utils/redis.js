954cf6d6337558c7636f5ef7f16edda058a56018
Imukua <ianmukuaa@gmail.com>
2024-03-12 11:09:52 +0300

    updated: class deffinitions

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,32 @@
-const Redis = require('redis');
+const { createClient } = require('redis');
 
 class RedisClient {
-  constructor(options) {
-    this.client = Redis.createClient(options);
-    this.client.on_connect('error', (err) => {
-      console.error('Redis Client Error:', err);
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+    this.connected = false;
+    this.client.on('connect', () => {
+      this.connected = true;
     });
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.connected;
   }
 
   async get(key) {
-    try {
-      const val = await this.client.get(key);
-      return val;
-    } catch (err) {
-      console.error('Redis got error:', err);
-      throw err;
-    }
+    const val = await this.client.get(key);
+    return val;
   }
 
   async set(key, value, duration) {
-    try {
-      await this.client.set(key, value, 'EX', duration);
-    } catch (err) {
-      console.error('Redis set error:', err);
-      throw err;
-    }
+    await this.client.set(key, value, 'EX', duration);
   }
 
   async del(key) {
-    try {
-      await this.client.del(key);
-    } catch (err) {
-      console.error('Redis del error:', err);
-      throw err;
-    }
+    await this.client.del(key);
   }
 }
 
