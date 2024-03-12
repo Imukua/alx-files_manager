34c13fd7ba5ba202af7f9e161d3b7c3ad615b8a7
Imukua <ianmukuaa@gmail.com>
2024-03-12 11:30:47 +0300

    restructured: utilised promisify for callbacks

--- utils/redis.js
+++ utils/redis.js
@@ -1,35 +1,40 @@
-const { createClient } = require('redis');
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => {
-      console.log(err);
+      console.log('Redis Client Error', err);
     });
+
     this.connected = false;
+
     this.client.on('connect', () => {
       this.connected = true;
     });
+
+    this.aSetX = promisify(this.client.setex).bind(this.client);
+    this.aGet = promisify(this.client.get).bind(this.client);
+    this.aDel = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
     return this.connected;
   }
 
-  async get(key) {
-    const val = await this.client.get(key);
-    return val;
+  set(key, value, expiry) {
+    this.aSetX(key, expiry, value);
   }
 
-  async set(key, value, duration) {
-    await this.client.set(key, value, 'EX', duration);
+  get(key) {
+    return this.aGet(key);
   }
 
-  async del(key) {
-    await this.client.del(key);
+  del(key) {
+    return this.aDel(key);
   }
 }
 
 const redisClient = new RedisClient();
-
-module.exports = redisClient;
+export default redisClient;
